7d4ea0f48fffb86f6df99fcb377ae1ad219e3284
zcong1993 <1005997919@qq.com>
2018-08-10 16:35:03 +0800

    update readme

--- utils/response.go
+++ utils/response.go
@@ -3,11 +3,21 @@ package utils
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"fmt"
+	"crypto/md5"
 )
+
+func md5Hash(str string) string {
+	data := []byte(str)
+	has := md5.Sum(data)
+
+	return fmt.Sprintf("%x", has)
+}
 
 // ResponseSvgWithCache send svg with needed headers
 func ResponseSvgWithCache(c *gin.Context, svg string) {
 	c.Header("Content-Type", "image/svg+xml;charset=utf-8")
 	c.Header("Cache-Control", "no-store, no-cache, must-revalidate")
-	c.String(http.StatusOK, string(svg))
+	c.Header("ETag", md5Hash(svg))
+	c.String(http.StatusOK, svg)
 }
