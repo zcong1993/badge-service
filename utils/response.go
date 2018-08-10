6fc098df4482d5ff85ae0a4e1ad658470f99d370
zcong1993 <1005997919@qq.com>
2018-08-10 16:51:05 +0800

    update

--- utils/response.go
+++ utils/response.go
@@ -1,16 +1,15 @@
 package utils
 
 import (
+	"crypto/md5"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"fmt"
-	"crypto/md5"
 )
 
 func md5Hash(str string) string {
 	data := []byte(str)
 	has := md5.Sum(data)
-
 	return fmt.Sprintf("%x", has)
 }
 
@@ -19,7 +18,6 @@ func ResponseSvgWithCache(c *gin.Context, svg string) {
 	c.Header("Content-Type", "image/svg+xml;charset=utf-8")
 	c.Header("Cache-Control", "no-cache")
 	c.Header("ETag", md5Hash(svg))
-	// Last-Modified: Wed, 21 Oct 2015 07:28:00 GMT
 	c.Header("Last-Modified", "Fri, 10 Aug 2018 08:42:57 GMT")
 	c.String(http.StatusOK, svg)
 }
