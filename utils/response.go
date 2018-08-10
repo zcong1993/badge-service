fff15b56f8b2df25c2e8042db040c99668445ee2
zcong1993 <1005997919@qq.com>
2018-08-10 16:46:16 +0800

    update readme

--- utils/response.go
+++ utils/response.go
@@ -17,9 +17,9 @@ func md5Hash(str string) string {
 // ResponseSvgWithCache send svg with needed headers
 func ResponseSvgWithCache(c *gin.Context, svg string) {
 	c.Header("Content-Type", "image/svg+xml;charset=utf-8")
-	c.Header("Cache-Control", "no-store, no-cache, must-revalidate")
+	c.Header("Cache-Control", "no-cache")
 	c.Header("ETag", md5Hash(svg))
 	// Last-Modified: Wed, 21 Oct 2015 07:28:00 GMT
-	c.Header("Last-Modified", "Wed, 21 Oct 2015 07:28:00 GMT")
+	c.Header("Last-Modified", "Fri, 10 Aug 2018 08:42:57 GMT")
 	c.String(http.StatusOK, svg)
 }
